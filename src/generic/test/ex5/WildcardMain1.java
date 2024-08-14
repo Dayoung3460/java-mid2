dc2bfaa518d947e3c86b95816066e803e08bbd0d
dayoung <kimdayoung771@gmail.com>
2024-08-14 23:06:45 +0900

    wildcard2

--- src/generic/test/ex5/WildcardMain1.java
+++ src/generic/test/ex5/WildcardMain1.java
@@ -15,8 +15,17 @@ public class WildcardMain1 {
         WildcardEx.printGenericV1(dogBox);
         WildcardEx.printWildcardV1(dogBox);
         WildcardEx.printGenericV2(dogBox);
+        // 컴파일 에러. 타입의 최대 부모는 Animal
+//        WildcardEx.printWildcardV2(objBox);
         Dog dog = WildcardEx.printAndReturn(dogBox);
+
+        catBox.set(new Cat("cat1", 200));
+        Cat cat = WildcardEx.printAndReturn(catBox);
         Animal animal = WildcardEx.printAndReturnWildcard(dogBox);
+
+        // return type이 Animal이기 때문에 Dog으로 받으려면 다운캐스팅 해줘야함\
+        // 이런 경우엔 와일드카드 말고 제네릭 사용하는게 나음
+        Dog dog2 = (Dog) WildcardEx.printAndReturnWildcard(dogBox);
         System.out.println("animal = " + animal);
     }
 }
