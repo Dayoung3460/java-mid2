184f0ac4716d1e162fbb15f85e815377ae507d77
dayoung <kimdayoung771@gmail.com>
2024-08-22 23:21:11 +0900

    Node and link3

---
+++ src/collection/link/NodeMain3.java
@@ -0,0 +1,60 @@
+package collection.link;
+
+public class NodeMain3 {
+    public static void main(String[] args) {
+        Node first = new Node("A");
+        first.next = new Node("B");
+        first.next.next = new Node("C");
+
+        System.out.println(first);
+
+        printAll(first);
+        System.out.println(getLastNode(first));
+
+        // 특정 인덱스 노드 조회
+        // 노드도 인덱스 개념이 있음
+        int index = 2;
+        Node index2Node = getNode(first, index);
+        System.out.println("index2Node = " + index2Node);
+
+        // add node
+        // node를 추가할 때 하나씩 추가: 배열과 다르게 메모리 낭비 하지 않음
+
+        add(first, "D");
+        System.out.println(first);
+        add(first, "E");
+        System.out.println(first);
+    }
+
+    private static void add(Node first, String param) {
+        Node lastNode = getLastNode(first);
+        lastNode.next = new Node(param);
+
+    }
+
+    private static Node getNode(Node first, int index) {
+        Node x = first;
+        for(int i = 0; i < index; i++) {
+            x = x.next;
+        }
+        return x;
+    }
+
+    private static void printAll(Node node) {
+        Node x = node;
+        while(x != null) {
+            System.out.println(x.item);
+            x = x.next;
+        }
+    }
+
+    private static Node getLastNode(Node node) {
+        Node x = node;
+        while(x.next != null) {
+            x = x.next;
+        }
+        return x;
+    }
+}
+
+// 각각의 노드를 연결(링크)해서 사용하는 자료 구조로 리스트를 만들 수 있음: 연결 리스트
