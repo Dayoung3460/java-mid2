0060f7e119aa77cf39e471f0f9433b59f2a962a7
dayoung <kimdayoung771@gmail.com>
2024-10-08 21:36:15 +0900

    Collection exercise

---
+++ src/collection/compare/test/shapeComparator.java
@@ -0,0 +1,21 @@
+package collection.compare.test;
+
+import java.util.Comparator;
+import java.util.HashMap;
+
+public class shapeComparator implements Comparator<Card> {
+  @Override
+  public int compare(Card o1, Card o2) {
+    HashMap<String, Integer> shapes = new HashMap<>();
+    shapes.put("s", 1);
+    shapes.put("h", 2);
+    shapes.put("d", 3);
+    shapes.put("c", 4);
+    if (o1.getNum() == o2.getNum()) {
+      return shapes.get(o1.getShape()).compareTo(shapes.get(o2.getShape()));
+    } else {
+      return 0;
+    }
+
+  }
+}
